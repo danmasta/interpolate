50d64129af8ee31567d5a206d502a197514d3721
Daniel Smith <dannmasta@gmail.com>
2023-11-12 15:22:10 -0800

    add support for formatter functions

--- bin/interpolate.js
+++ bin/interpolate.js
@@ -18,6 +18,8 @@ Options:
 --throw           -t - If true will throw errors for missing parameters
 --default         -d - What value to use as default when a parameter is not found
 --replace-missing -r - If false will not replace variables that are undefined
+--format          -f - Enables or disables formatter functions
+--delimeter          - What delimiter to use for parsing formatter names
 --help            -h - Show this help message
 
 Examples:
@@ -39,7 +41,9 @@ function runWithArgv () {
         default: 'd',
         help: 'h',
         regex: null,
-        replaceMissing: 'r'
+        replaceMissing: 'r',
+        format: 'f',
+        delimiter: null
     });
 
     let interpolate = new Interpolate(opts);
