b79a1ac1f64e1781ed240f724f4d591ee77243ca
Daniel Smith <dannmasta@gmail.com>
2024-02-03 12:39:10 -0800

    update tests setup

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const chai = require('chai');
 
-beforeEach(function () {
-    global.path = path;
-    global.assert = chai.assert;
-    global.expect = chai.expect;
-    global.should = chai.should();
+beforeEach(() => {
+    return import('chai').then(chai => {
+        global.assert = chai.assert;
+        global.expect = chai.expect;
+        global.should = chai.should();
+        global.path = path;
+    });
 });
